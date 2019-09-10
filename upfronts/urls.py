4e5f371245fbee4cf43884022c128ab5877ae77c
juan.elias <juan.elias@eventbrite.com>
2019-09-10 12:38:18 -0300

    Add basic template inheritance

--- upfronts/urls.py
+++ upfronts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('app.urls')),
-    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/login/$', LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^accounts/logout/$', LogoutView.as_view(), name='logout'),
     url('', include('social_django.urls', namespace='social')),
 ]
