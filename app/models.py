63d6a23b5a2b12a56d06be444590566d133faf05
Walter Bucolo <walter.bucolo@eventbrite.com>
2019-09-17 11:43:41 -0300

    upfront model added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS = [
+    ('COMMITED/APPROVED', 'COMMITED/APPROVED'),
+    ('PENDING', 'PENDING'),
+]
+
+
+class Upfront(models.Model):
+    is_recoup = models.BooleanField()
+    status = models.CharField(max_length=80, choices=STATUS)
+    organizer = models.CharField(max_length=80)
+    account_name = models.CharField(max_length=80)
+    email_organizer = models.EmailField()
+    upfront_projection = models.DecimalField(max_digits=19, decimal_places=10)
+    contract_signed_date = models.DateField()
+    maximum_payment_date = models.DateField()
+    payment_date = models.DateField()
+    recoup_amount = models.DecimalField(max_digits=19, decimal_places=10)
+    gtf = models.DecimalField(max_digits=19, decimal_places=10)
+    gts = models.DecimalField(max_digits=19, decimal_places=10)
