d7a984a303bd485abe5ab939fb84216f42fd5cdd
Dávila Agustín <agustin.davila@eventbrite.com>
2019-09-18 11:10:30 -0300

    Templates structure reorganized. Deleted unused template

--- upfronts/urls.py
+++ upfronts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^accounts/login/$', LoginView.as_view(template_name='registration/login.html'), name='login'),
     url(r'^accounts/logout/$', LogoutView.as_view(), name='logout'),
     url('', include('social_django.urls', namespace='social')),
     url(r'', include('app.urls')),
