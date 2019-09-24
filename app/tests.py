15707c1062883b4284a9727adce123c81f59b9d5
Dávila Agustín <agustin.davila@eventbrite.com>
2019-09-24 12:34:26 -0300

    More test were add

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,11 @@
-from django.test import Client, TestCase
+from django.contrib.auth.models import AnonymousUser
+from django.test import Client, TestCase, RequestFactory
 from django.urls import reverse
 from .models import Upfront
 from .views import UpfrontFilter
+from .views import download_csv
+import csv
+import io
 
 
 class RedirectTest(TestCase):
@@ -63,3 +67,61 @@ class FilterTest(TestCase):
         self.assertIn(up1, result)
         self.assertIn(up2, result)
         self.assertNotIn(up3, result)
+
+
+class TestCsv(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_status_code_200(self):
+        request = self.factory.get(reverse('download_csv'))
+        request.user = AnonymousUser()
+        response = download_csv(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_download_csv(self):
+        request = self.factory.get(reverse('download_csv'))
+        request.user = AnonymousUser()
+        response = download_csv(request)
+        self.assertEqual('text/csv', dict(response.items())['Content-Type'])
+        self.assertIn('-upfronts.csv', dict(response.items())['Content-Disposition'])
+
+    def test_download_csv_with_info(self):
+        expected_upfront_dict = {
+            'is_recoup': 'True',
+            'status': 'COMMITED/APPROVED',
+            'organizer': 'IDO',
+            'account_name': 'EDA',
+            'email_organizer': 'juan@eventbrite.com',
+            'upfront_projection': '77777.0000000000',
+            'contract_signed_date': '2019-04-04',
+            'maximum_payment_date': '2019-05-30',
+            'payment_date': '2019-05-05',
+            'recoup_amount': '55555.0000000000',
+            'gtf': '7000.0000000000',
+            'gts': '100000.0000000000',
+        }
+        Upfront.objects.create(
+            is_recoup=True,
+            status='COMMITED/APPROVED',
+            organizer='IDO',
+            account_name='EDA',
+            email_organizer='juan@eventbrite.com',
+            upfront_projection=77777,
+            contract_signed_date='2019-04-04',
+            maximum_payment_date='2019-05-30',
+            payment_date='2019-05-05',
+            recoup_amount=55555,
+            gtf=100000,
+            gts=7000,
+        )
+        request = self.factory.get(reverse('download_csv'))
+        request.user = AnonymousUser()
+        response = download_csv(request)
+        response_decode = response.content.decode('utf-8')
+        csv_list = csv.reader(io.StringIO(response_decode))
+        for row in csv_list:
+            csv_set = set(row)
+        expected_csv_set = set(list(expected_upfront_dict.values()))
+        self.assertEqual(csv_set, expected_csv_set)
