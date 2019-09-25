26748e9ac87051a7e5cac7014385c23d51baea22
walterbucolo-eb <53877957+walterbucolo-eb@users.noreply.github.com>
2019-09-25 10:30:29 -0300

    Revise model (#13)
    * contract model added
    
    * Installment model added
    
    * contract fk to installment added
    
    * Installment condition added
    
    * add STATUS constant to installment_data dict
    
    * Decimal_places changed from 10 to 4
    
    * Refactor replace upfront for Installment model
    
    * Redirect to login fixed
    
    * Test for InstallmentTableView added
    
    * Test for InstallmentTableView added
    
    * Test for InstallmentTableView added
    
    * fix linting problems
    
    * commented code was removed and changes way to use import
    
    * Fix linting issues

--- app/models.py
+++ app/models.py
@@ -1,22 +1,26 @@
 from django.db import models
+from . import STATUS, INSTALLMENT_CONDITIONS
 
 
-STATUS = [
-    ('COMMITED/APPROVED', 'COMMITED/APPROVED'),
-    ('PENDING', 'PENDING'),
-]
+class Contract(models.Model):
+    organizer_account_name = models.CharField(max_length=80)
+    organizer_email = models.EmailField()
+    signed_date = models.DateField()
 
 
-class Upfront(models.Model):
+class Installment(models.Model):
+    contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
     is_recoup = models.BooleanField()
     status = models.CharField(max_length=80, choices=STATUS)
-    organizer = models.CharField(max_length=80)
-    account_name = models.CharField(max_length=80)
-    email_organizer = models.EmailField()
-    upfront_projection = models.DecimalField(max_digits=19, decimal_places=10)
-    contract_signed_date = models.DateField()
+    upfront_projection = models.DecimalField(max_digits=19, decimal_places=4)
     maximum_payment_date = models.DateField()
     payment_date = models.DateField()
-    recoup_amount = models.DecimalField(max_digits=19, decimal_places=10)
-    gtf = models.DecimalField(max_digits=19, decimal_places=10)
-    gts = models.DecimalField(max_digits=19, decimal_places=10)
+    recoup_amount = models.DecimalField(max_digits=19, decimal_places=4)
+    gtf = models.DecimalField(max_digits=19, decimal_places=4)
+    gts = models.DecimalField(max_digits=19, decimal_places=4)
+
+
+class InstallmentCondition(models.Model):
+    condition_name = models.CharField(max_length=80, choices=INSTALLMENT_CONDITIONS)
+    installment = models.ForeignKey(Installment, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
