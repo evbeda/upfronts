84b9ace614c4e47575bbdb0b0e8dfe407669ef7a
walterbucolo-eb <53877957+walterbucolo-eb@users.noreply.github.com>
2019-10-22 15:00:24 -0300

    Html structure fixed (#36)
    * Fix styling errors
    
    * Html structure was fixed in all html files

---
+++ app/static/js/add_contracts.js
@@ -0,0 +1,8 @@
+$( function() {
+    $('#datepicker_from').datepicker({
+        uiLibrary: 'bootstrap4',
+    });
+    $('#datepicker_to').datepicker({
+        uiLibrary: 'bootstrap4',
+    });
+})
