f15443eaeabbb7bd0792022a0a6e661a923eda5f
juan.elias <juan.elias@eventbrite.com>
2020-01-21 14:33:00 -0300

    Fix settings

--- settings/base.py
+++ settings/base.py
@@ -93,7 +93,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'upfronts.wsgi.application'
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- settings/dev.py
+++ settings/dev.py
@@ -10,13 +10,6 @@ SOCIAL_AUTH_EVENTBRITE_SECRET = get_env_variable(
     'SOCIAL_AUTH_EVENTBRITE_SECRET',
 )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa
-    }
-}
-
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = get_env_variable('DROPBOX_OAUTH2_TOKEN')
 DROPBOX_ROOT_PATH = '/Backup_Files'
